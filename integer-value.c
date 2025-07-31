5698aef01249591d03d763501ffd8ec9aeb55a00
suhanaa29 <aparnasingh29062006@gmail.com>
2025-07-31 07:59:57 +0530

    Add three C programs, update README, and LICENSE

---
+++ integer-value.c
@@ -0,0 +1,19 @@
+// C program to take an integer
+// as input and print it
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+    // Declare the variables
+    int num;
+
+    // Input the integer
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    // Display the integer
+    printf("Entered integer is: %d", num);
+
+    return 0;
+}
